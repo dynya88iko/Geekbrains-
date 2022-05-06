990e6f1f7a666bd84ad3ba921d5b1f796ad78e51
dynya88iko <ir999@yandex.ru>
2022-05-06 23:00:15 +0300

    weefswvhshshhsj

--- Djoni.java
+++ Djoni.java
@@ -1,67 +1,122 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Djoni {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
     public static void main(String[] args) {
-        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println();
-        doTask1(arr1);
-        System.out.println();
-        doTask2(100);
-        System.out.println();
-        doTask3(arr3);
-        System.out.println();
-        doTask4();
-        System.out.println();
-        doTask5(5, 1);
-    }
-
-    public static void doTask1(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
             }
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void doTask2(int size) {
-        int[] arr = new int[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void doTask3(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) {
-                arr[i] = arr[i] * 2;
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
             }
-            System.out.print(arr[i] + " ");
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static boolean checkWin(char symb) {
+        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return
+                true;
+        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return
+                true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return
+                true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return
+                true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return
+                true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return
+                true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return
+                true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return
+                true;
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y +
+                1));
+        map[y][x] = DOT_O;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
         }
     }
-    public static void doTask4 () {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}};
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i == j) {
-                    arr[i][j] = 1;
-                }
-                System.out.print(arr[i][j] + " ");
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
-    }
-    public static void doTask5(int len, int initialValue){
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = initialValue;
-            System.out.print("[" + i + "]" + arr[i] + " ");
-        }
+
+        System.out.println();
     }
 }
 
