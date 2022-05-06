57ff79f0db62e6dfbefa0584537665a8703cb0be
dynya88iko <ir999@yandex.ru>
2022-05-06 22:12:26 +0300

    weefswvhshshhsj

--- Djoni.java
+++ Djoni.java
@@ -1,44 +1,67 @@
 public class Djoni {
-
     public static void main(String[] args) {
-
-        System.out.println(doThree(5, 6));
-        System.out.println("  ");
-        System.out.println(doFour(4));
-        System.out.println("  ");
-        System.out.println(doFive(3));
-        System.out.println("  ");
-        four(3, "Привет,как дела?");
-        System.out.println(isLeapYearBool(1986));
-
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println();
+        doTask1(arr1);
+        System.out.println();
+        doTask2(100);
+        System.out.println();
+        doTask3(arr3);
+        System.out.println();
+        doTask4();
+        System.out.println();
+        doTask5(5, 1);
     }
 
-    public static boolean doThree(int a, int b) {
-        int sum = a + b;
-        if (sum > 10 && sum < 20) return true;
-        else return false;
-    }
-
-    public static boolean doFour(int a) {
-        if(a > 0)return false;
-        else return true;
-
-    }
-
-    public static boolean doFive(int a) {
-        if (a < 0) return true;
-        return false;
-    }
-
-    public static void four(int a, String string) {
-        for (int i = a; i > 0; i--) {
-            System.out.println(string);
-        }}
-
-    public static boolean isLeapYearBool ( int year) {
-            if (year%4==0 && year%100 !=0 || year%400==0) return true;
-            return false;
+    public static void doTask1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
         }
+    }
 
+    public static void doTask2(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void doTask3(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void doTask4 () {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void doTask5(int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+    }
 }
 
