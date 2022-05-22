6569170c562643dcc8a08e7d6dc984ba8f669d89
dynya88iko <ir999@yandex.ru>
2022-05-22 12:18:01 +0300

    weefswvhshshhsj

--- Djoni.java
+++ Djoni.java
@@ -1,44 +1,89 @@
-public class Djoni {
+import java.util.Random;
+
+public class Djoni
+{
     public static void main(String[] args) {
-        Employee[] empCorp = new Employee[5];
-        empCorp[0] = new Employee("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312",50000,41);
-        empCorp[1] = new Employee("Petrov Petr", "Creator", "petrov@mailbox.com", "892312314",50000,32);
-        empCorp[2] = new Employee("Sidorov Sidr", "Water pot", "sidorov@mailbox.com", "892312315",10000,43);
-        empCorp[3] = new Employee("Pupkina Nadegda", "EnvelopeLicker", "pupkina@mailbox.com", "892312316",5000,18);
-        empCorp[4] = new Employee("Juk J", "GrinderPencil", "juk@mailbox.com", "892312317",3000,21);
-        for (Employee employee : empCorp)
-            if (employee.getAge()>40)
-                System.out.println(employee);
+        Cat cat1 = new Cat("Barsik");
+        Cat cat2 = new Cat("Pushok");
 
+        Dog dog1 = new Dog("Bobik");
+        Dog dog2 = new Dog("Sharik");
+
+        cat1.info();
+        cat2.info();
+
+        System.out.println();
+
+        dog1.info();
+        dog2.info();
+
+        System.out.println();
+
+        dog1.run(400);
+        dog1.swim(5);
+        dog1.jump(0.3);
+
+        System.out.println();
+
+        cat1.run(1700);
+        cat1.swim(5);
+        cat1.jump(1.7);
+    }
+
+    public static class Animals {//super class
+        protected String name;
+        protected String type;
+        protected int maxRun;
+        protected int maxSwim;
+        protected double maxJump;
+        public Random random = new Random();
+
+        public Animals(String name){
+            this.name = name; // при создании объекта нужно ввести имя
+        }
+
+        public void run(int dist) { // выводит инфо об объекте относительно действия
+            if (this.maxRun >= dist) System.out.println(this.type + " " + this.name + " run " + dist + " m.");
+            else System.out.println(this.type + " " + this.name + " can't run so far.");
+        }
+
+        public void swim(int dist) {// выводит инфо об объекте относительно действия
+            if (this.maxSwim >= dist) System.out.println(this.type + " " + this.name + " swim " + dist + " m.");
+            else System.out.println(this.type + " " + this.name + " can't swim so far.");
+        }
+
+        public void jump(double height) {// выводит инфо об объекте относительно действия
+            if (this.maxJump >= height)
+                System.out.println(this.type + " " + this.name + " jump " + height + " m.");
+            else System.out.println(this.type + " " + this.name + " can't jump so hi.");
+        }
+
+        public void info() { // выводит сведения об объекте в разрезе дейсвтий
+            System.out.println(this.type + " " + this.name + " Run: " + this.maxRun + " m., Swim: " + this.maxSwim + " m., Jump: " + this.maxJump + " m.");
+        }
+    }
+
+    public static class Cat extends Animals {//наследующий класс
+        public Cat(String name) {
+            super(name);// наследуюемый признак
+            this.maxJump = 2.0;
+            this.maxRun = random.nextInt(45) + 155;
+            this.type = "Cat";
+        }
+
+        @Override
+        public void swim(int dist) {
+            System.out.println("Cat's no swim!");
+        }
+    }
+
+    public static class Dog extends Animals {//наследующий класс
+        public Dog(String name) {
+            super(name);
+            this.type = "Dog";
+            this.maxRun = random.nextInt(300) + 300;
+            this.maxSwim = random.nextInt(6) + 5;
+            this.maxJump = Math.random() * 0.7 + 0.1;
+        }
     }
 }
-class Employee {
-    private String name;
-    private String position;
-    private String email;
-    private String phone;
-    private int salary;
-    private int age;
-
-    public Employee(String name, String position, String email, String phone, int salary, int age) {
-        this.name = name;
-        this.position = position;
-        this.email = email;
-        this.phone = phone;
-        this.salary = salary;
-        this.age = age;
-    }
-    public int getAge() {
-        return age;
-    }
-    @Override
-    public String toString(){
-        return(name+ "\n-"
-                +position+ "\n-"
-                +email+ "\n-"
-                +phone+ "\n-"
-                +salary+ "\n-"
-                +age);
-    }
-}
-
